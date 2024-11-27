023d5c05ed611611701054de41a106a2aa3bf4f8
Michael Abad <jun.abad@my.utsa.edu>
2024-11-26 22:13:46 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
+import Todo from './models/todo';
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const NewTodo = new Todo(todoText);
+
+    setTodos(prevTodos => {
+      return prevTodos.concat(NewTodo);
+    });
+  };
+
+  const onRemoveTodoHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
+
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={onRemoveTodoHandler} />
+    </div>
+  );
+}
+
+export default App;
